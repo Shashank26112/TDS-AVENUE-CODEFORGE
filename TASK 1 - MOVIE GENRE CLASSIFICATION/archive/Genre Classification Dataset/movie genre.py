5b1aeb44d4bde6e9d782402b13af5efac0866d45
Shashank KATTI <90169675+Shashank26112@users.noreply.github.com>
2024-08-13 19:29:02 +0530

    Update movie genre.py

--- TASK 1 - MOVIE GENRE CLASSIFICATION/archive/Genre Classification Dataset/movie genre.py
+++ TASK 1 - MOVIE GENRE CLASSIFICATION/archive/Genre Classification Dataset/movie genre.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the training data
-train_path = "C:/Users/hp/Desktop/ML internhip/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/train_data.txt"
+train_path = "C:/Users/hp/Desktop/ML internship/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/train_data.txt"
 train_data = pd.read_csv(train_path, sep=':::', names=['Title', 'Genre', 'Description'], engine='python')
 train_data.columns
 train_data.head(10)
 train_data.isnull().sum()
 train_data.info()
 train_data.describe()
-test_path = "C:/Users/hp/Desktop/ML internhip/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/test_data.txt"
+test_path = "C:/Users/hp/Desktop/ML  internship/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/test_data.txt"
 test_data = pd.read_csv(test_path, sep=':::', names=['Id', 'Title', 'Description'], engine='python')
 test_data.columns
 test_data.head(20)
@@ -19,7 +19,7 @@ test_data.info()
 test_data.describe()
 
 # Load test data solution
-test_solution_path = "C:/Users/hp/Desktop/ML internhip/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/test_data_solution.txt"
+test_solution_path = "C:/Users/hp/Desktop/ML internship/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/test_data_solution.txt"
 test_solution = pd.read_csv(test_solution_path, sep=":::", names=['Id', 'Title', 'Gener', 'Description'], engine="python")
 test_solution.head(10)
 genre_counts = train_data['Genre'].value_counts()
