30dbef0b62fe1cd9442248f0f5a503d63ef50bbc
Shashank KATTI <90169675+Shashank26112@users.noreply.github.com>
2024-08-13 19:35:05 +0530

    Update movie genre1.py

--- TASK 1 - MOVIE GENRE CLASSIFICATION/archive/Genre Classification Dataset/movie genre1.py
+++ TASK 1 - MOVIE GENRE CLASSIFICATION/archive/Genre Classification Dataset/movie genre1.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the training data
-train_path = "C:/Users/hp/Desktop/ML internship/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/train_data.txt"
-train_data = pd.read_csv(train_path, sep=':::', names=['Title', 'Genre', 'Description'], engine='python')
+train_path = "C:/Users/hp/Desktop/ML internship/TASK 1 - MOVIE GENERE CLASSIFICATION/archive\Genre Classification Dataset/train_data.txt"
+train_data = pd.read_csv(train_path, sep=':::', names=['Title', 'Genere', 'Description'], engine='python')
 train_data.columns
 train_data.head(10)
 train_data.isnull().sum()
 train_data.info()
 train_data.describe()
-test_path = "C:/Users/hp/Desktop/ML  internship/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/test_data.txt"
+test_path = "C:/Users/hp/Desktop/ML  internship/TASK 1 - MOVIE GENERE CLASSIFICATION/archive\Genre Classification Dataset/test_data.txt"
 test_data = pd.read_csv(test_path, sep=':::', names=['Id', 'Title', 'Description'], engine='python')
 test_data.columns
 test_data.head(20)
@@ -19,17 +19,16 @@ test_data.info()
 test_data.describe()
 
 # Load test data solution
-test_solution_path = "C:/Users/hp/Desktop/ML internship/TASK 1 - MOVIE GENRE CLASSIFICATION/archive\Genre Classification Dataset/test_data_solution.txt"
-test_solution = pd.read_csv(test_solution_path, sep=":::", names=['Id', 'Title', 'Gener', 'Description'], engine="python")
+test_solution_path = "C:/Users/hp/Desktop/ML internship/TASK 1 - MOVIE GENERE CLASSIFICATION/archive\Genre Classification Dataset/test_data_solution.txt"
+test_solution = pd.read_csv(test_solution_path, sep=":::", names=['Id', 'Title', 'Genere', 'Description'], engine="python")
 test_solution.head(10)
-genre_counts = train_data['Genre'].value_counts()
-genre_counts = genre_counts.sort_values(ascending=False)
+genere_counts = train_data['Genre'].value_counts()
+genere_counts = genre_counts.sort_values(ascending=False)
 plt.figure(figsize=(12, 6))
-sns.barplot(x=genre_counts.index, y=genre_counts.values, palette='viridis')
-plt.title('Distribution of Genres in Training Data', fontsize=16)
-plt.xlabel('Genre', fontsize=12)
+sns.barplot(x=genere_counts.index, y=genere_counts.values, palette='viridis')
+plt.title('Distribution of Generes in Training Data', fontsize=16)
+plt.xlabel('Genere', fontsize=12)
 plt.ylabel('Count', fontsize=12)
 plt.xticks(rotation=45, ha='right')
 plt.grid(True, axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
-plt.show()
